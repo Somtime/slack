809ba9f015222ff9147bebefda8a24e138eed952
Somtime <rghee0821@gmail.com>
2024-01-29 22:24:00 +0900

    Datasource, QueryRunner 를 이용한 Transaction 처리 찾아보니 Transaction 을 Decorator 로 만들어서 사용할 수도 있을 것 같음 하지만 강의에선 데코레이터로 사용 시 DI에 문제가 있다는 언급함 이후에 데코레이터를 생성해서 적용해보고 사용 시 문제가 될 지 확인해보는 것도 좋을 것 같음

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,29 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    private dataSource: DataSource,
   ) {}
 
   async join(email: string, nickname: string, password: string) {
-    const user = await this.usersRepository.findOne({
+    const queryRunner = this.dataSource.createQueryRunner();
+    queryRunner.connect();
+    queryRunner.startTransaction();
+
+    const user = await queryRunner.manager.getRepository(Users).findOne({
       where: {
         email,
       },
@@ -21,13 +32,33 @@ export class UsersService {
     if (user) throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const returned = await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
+    try {
+      const savedUser = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
 
-    return returned;
+      await queryRunner.manager.getRepository(WorkspaceMembers).save({
+        UserId: savedUser.id,
+        WorkspaceId: 1,
+      });
+
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: savedUser.id,
+        ChannelId: 1,
+      });
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      console.log(error);
+      await queryRunner.rollbackTransaction();
+      throw new HttpException('UsersService.join() transaction error', 403);
+    } finally {
+      await queryRunner.release();
+    }
+
+    return true;
   }
 
   async findByEmail(email: string) {
